f5f4b4be0bc70d50a961a062f5b0dc3132c5619d
Rillian Diello <rillian@favish.com>
2024-12-10 22:18:27 -0500

    config style components

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import ReactDOM from 'react-dom/client';
 import Home from './templates/App';
+import { GlobalStyle } from './styles/global-styles';
+import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  colors: {
+    mainBg: 'gray',
+    secondaryBg: 'white',
+  },
+  fonts: {},
+  spacings: {},
+};
