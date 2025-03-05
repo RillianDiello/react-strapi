e900069226215f15ee2f083b2b57e1bc1d10095d
Rillian Diello <rillian@favish.com>
2025-03-04 23:42:57 -0400

    react-dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
-import ReactDOM from 'react-dom/client';
 import Home from './templates/App';
 import { GlobalStyle } from './styles/global-styles';
 import { theme } from './styles/theme';
