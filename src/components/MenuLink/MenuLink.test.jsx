9692996685be093cf82cde6ac0d5f413b4f1d7ef
Rillian Diello <rillian@favish.com>
2025-03-06 23:27:37 -0400

    MenuLink

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,65 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { MenuLink } from '.';
+
+describe('<MenuLink />', () => {
+  it('should render a link', () => {
+    renderTheme(<MenuLink link="http//localhost">Children</MenuLink>);
+    expect(screen.getByRole('link', { name: 'Children' })).toBeInTheDocument();
+  });
+  it('should render open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http//localhost" newTab={true}>
+        Children
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
+  });
+  it('should render with newTab false', () => {
+    renderTheme(
+      <MenuLink link="http//localhost" newTab={false}>
+        Children
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' }))
+      .toMatchInlineSnapshot(`
+      .c0 {
+        display: block;
+        text-decoration: none;
+        font-size: 1.6rem;
+        padding: 1.6rem 8rem;
+        color: #0A1128;
+        position: relative;
+      }
+
+      .c0::after {
+        content: '';
+        position: absolute;
+        bottom: 0.75rem;
+        left: 50%;
+        width: 0;
+        height: 0.2rem;
+        background: #dc143c;
+        transition: all 300ms ease-in-out;
+      }
+
+      .c0:hover::after {
+        width: 25%;
+        left: 50%;
+      }
+
+      <a
+        class="c0"
+        href="http//localhost"
+        target="_self"
+      >
+        <h1>
+          Children
+        </h1>
+      </a>
+    `);
+  });
+});
---
+++ src/components/MenuLink/index.jsx
@@ -0,0 +1,15 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+export const MenuLink = ({ children, link, newTab = false }) => {
+  const target = newTab ? '_blank' : '_self';
+  return (
+    <Styled.Container href={link} target={target}>
+      <h1>{children}</h1>
+    </Styled.Container>
+  );
+};
+MenuLink.propTypes = {
+  children: P.node,
+  link: P.string.isRequired,
+  newTab: P.bool,
+};
