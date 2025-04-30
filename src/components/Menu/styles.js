0be6965c38e48ba9ed4e173c4a4d4d2702c108db
Rillian Diello <rillian@favish.com>
2025-04-29 23:19:13 -0400

    menu style

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -2,8 +2,14 @@ import styled, { css } from 'styled-components';
 import { Container as SectionContainer } from '../SectionContainer/styles';
 import { Container as Heading } from '../Heading/styles';
 
+const menuVisible = () => css`
+  visibility: visible;
+  opacity: 1;
+  transition: all 300ms ease-in-out;
+  `;
+
 export const Container = styled.div`
-  ${({ theme }) => css`
+  ${({ theme, visible }) => css`
     position: fixed;
     z-index: 5;
     top: 0;
@@ -28,6 +34,7 @@ export const Container = styled.div`
       height: 100vh;
       visibility: hidden;
       opacity: 0;
+      ${visible && menuVisible()}
 
       > ${SectionContainer} {
         display: grid;
@@ -56,13 +63,37 @@ export const MenuContainer = styled.div`
     @media ${theme.media.lteMedium} {
       display: block;
       text-align: center;
-      padding: ${theme.spacings.xxlarge} 0;
+      padding: ${theme.spacings.xxlarge};
     }
   `}
 `;
 
 export const ButtonContainer = styled.button`
-  ${() => css`
+  ${({ theme, visible }) => css`
+    z-index: 6;
+    position: fixed;
+    top: 2rem;
+    right: 2rem;
+    width: 4rem;
+    height: 1.5rem;
+    background: ${theme.colors.primaryColor};
+    color: ${theme.colors.white};
+    border: none;
+    pointer-events: ${visible ? 'none' : 'all'};
 
+    display: none;
+    @media ${theme.media.lteMedium} {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      padding: ${theme.spacings.medium} 0;
+    }
   `}
+
+  > svg {
+    width: 2.5rem;
+    height: 2.5rem;
+    color: inherit;
+  }
 `;
