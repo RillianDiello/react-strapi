97d3709b2271a51e32cc2b9c9e76c432126c5b98
Rillian Diello <rillian@favish.com>
2025-04-29 23:18:05 -0400

    Menu

--- src/components/Menu/Menu.test.jsx
+++ src/components/Menu/Menu.test.jsx
@@ -2,9 +2,24 @@ import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
 import { Menu } from '.';
 
+import linksMock from '../NavLinks/mock';
+const logoData = {
+  text: 'Logo',
+  link: '#target',
+};
+
 describe('<Menu />', () => {
   it('should render', () => {
-    renderTheme(<Menu>Children</Menu>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+    const { container } = renderTheme(
+      <Menu links={linksMock} logoData={logoData}>
+        Children
+      </Menu>,
+    );
+    expect(screen.getByRole('heading', { name: 'Logo' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('navigation', { name: 'Main menu' }),
+    ).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
   });
 });
